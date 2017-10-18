9d09ee90b43f5688aff28af6158ddc517c7def0f
Peter J. Kohler, PhD <pjkohl3r@gmail.com>
2017-10-18 16:10:17 -0700

    adopt left/right hand convention, rather than bad/good

--- generate_roiFig.m
+++ generate_roiFig.m
@@ -4,91 +4,107 @@ addpath(genpath(sprintf('%s/git/schlegel/matlab_lib',codeFolder)));
 
 SUBJECTS_DIR = '/Volumes/Denali_4D2/kohler/fMRI_EXP/Handmovement/surfaces';
 DATA_DIR = '/Volumes/Denali_4D2/kohler/fMRI_EXP/Handmovement/data';
-SUB_ID='LS';
 
-badFile = sprintf('%s/%s/badCombined.nii.gz',DATA_DIR,SUB_ID);
-goodFile = sprintf('%s/%s/goodCombined.nii.gz',DATA_DIR,SUB_ID);
-rhFile = sprintf('%s/%s/rh.wang_atlas_hand.nii.gz',DATA_DIR,SUB_ID);
-lhFile = sprintf('%s/%s/lh.wang_atlas_hand.nii.gz',DATA_DIR,SUB_ID);
+subList = {'LS','AO','JZ'};
+rightThresh = [3.09,2.86,2.99];
+leftThresh = [3.25];
 
-badMat = NIfTI.Read(badFile,'return','data');
-goodMat = NIfTI.Read(goodFile,'return','data');
-badMat = squeeze(badMat(:,:,:,1,2)); % get z-scores
-goodMat = squeeze(goodMat(:,:,:,1,2)); % get z-scores
+doLeft = false;
+for s=1:length(subList)
+    SUB_ID=subList{s};
+    if strcmp(subList{s},'LS')
+        doLeft = true;
+    else
+        doLeft = false;
+    end
+    % rois
+    rhFile = sprintf('%s/%s/rh.wang_atlas_hand.nii.gz',DATA_DIR,SUB_ID);
+    lhFile = sprintf('%s/%s/lh.wang_atlas_hand.nii.gz',DATA_DIR,SUB_ID);
+    rhROI = NIfTI.Read(rhFile,'return','data');
+    lhROI = NIfTI.Read(lhFile,'return','data');
+    
+    % right
+    rightFile = sprintf('%s/%s/rightCombined.nii.gz',DATA_DIR,SUB_ID);
+    rightMat = NIfTI.Read(rightFile,'return','data');
+    rightMat = squeeze(rightMat(:,:,:,1,2)); % get z-scores
+    
+    % left
+    if doLeft
+        leftFile = sprintf('%s/%s/leftCombined.nii.gz',DATA_DIR,SUB_ID);
+        leftMat = NIfTI.Read(leftFile,'return','data');
+        leftMat = squeeze(leftMat(:,:,:,1,2)); % get z-scores
+    else
+    end
 
-
-rhROI = NIfTI.Read(rhFile,'return','data');
-lhROI = NIfTI.Read(lhFile,'return','data');
-
-for z=1:18
-    percSig(z,1,1) = length(find(lhROI == z & goodMat > 3.25 ))/length(find(lhROI == z));
-    percSig(z,2,1) = length(find(lhROI == z & badMat > 3.09 ))/length(find(lhROI == z));
-    percSig(z,1,2) = length(find(rhROI == z & goodMat > 3.25 ))/length(find(rhROI == z));
-    percSig(z,2,2) = length(find(rhROI == z & badMat > 3.09 ))/length(find(rhROI == z));
-    meanZ(z,1,1) = mean(goodMat(lhROI == z));
-    meanZ(z,2,1) = mean(badMat(lhROI == z));
-    meanZ(z,1,2) = mean(goodMat(rhROI == z));
-    meanZ(z,2,2) = mean(badMat(rhROI == z));
+    for z=1:18
+        percSig{s}(z,1,1) = length(find(lhROI == z & rightMat > rightThresh(s) ))/length(find(lhROI == z));
+        percSig{s}(z,1,2) = length(find(rhROI == z & rightMat > rightThresh(s) ))/length(find(rhROI == z));
+        meanZ{s}(z,1,1) = mean(rightMat(lhROI == z));
+        meanZ{s}(z,1,2) = mean(rightMat(rhROI == z));
+        if doLeft
+            percSig{s}(z,2,1) = length(find(lhROI == z & leftMat > leftThresh(s) ))/length(find(lhROI == z));
+            percSig{s}(z,2,2) = length(find(rhROI == z & leftMat > leftThresh(s) ))/length(find(rhROI == z));
+            meanZ{s}(z,2,1) = mean(leftMat(lhROI == z));
+            meanZ{s}(z,2,2) = mean(leftMat(rhROI == z));
+        else
+        end
+    end
 end
 
 %% MAKE FIGURES
-% PERC FIGURE
 close all;
-fSize = 12;
+fSize(1) = 12;
+fSize(2) = 18;
+lWidth = 2;
 includeROIs = {'V1' 'V2' 'V3' 'hV4' 'VO1' 'VO2','PHC1','PHC2',...
             'TO1','LO2','LO1','V3A','V3B','IPS0','IPS1','IPS2','IPS3','SPL1'};
-gcaOpts = {'tickdir','out','ticklength',[0.02,0.02],'box','off','fontsize',fSize,'fontname','Helvetica','linewidth',lWidth};
-figure;
-for z = 1:2
-    subplot(1,2,z);
-    hold on;
-    plot(percSig(:,:,z),'-o');
-    ylim([-.1,.6]); xlim([.5,18.5]);
-    if z==1
-        title('LH');
-        legend('good','bad')
-        legend boxoff
-        ylabel('significant voxels (%)','fontname','Helvetica','fontsize',fSize);
-    else
-        title('RH');
-    end
-    set(gca,gcaOpts{:},'xtick',1:18,'xticklabel',includeROIs,'ytick',0:.1:.6);
-    rotateXLabels(gca,45);
-    hold off
-end
-set(gcf, 'units', 'centimeters'); % make figure size units centimeters
-oldPos = get(gcf, 'pos');
-newPos = oldPos;
-newPos(3) = 30;
-newPos(4) = 10;
-set(gcf, 'pos',newPos);
-figName = sprintf('percROI_%s',SUB_ID);
-export_fig([figName,'.pdf'],'-pdf','-transparent',gcf);
+gcaOpts = {'tickdir','out','ticklength',[0.02,0.02],'box','off','fontsize',fSize(1),'fontname','Helvetica','linewidth',lWidth};
+meanFig = figure;
+percFig = figure;
 
-% MEAN FIGURE
-figure;
-for z = 1:2
-    subplot(1,2,z);
-    hold on;
-    plot(meanZ(:,:,z),'-o');
-    ylim([-4,4]); xlim([.5,18.5]);
-    if z==1
-        title('LH');
-        legend('good','bad')
-        legend boxoff
-        ylabel('average Z-score','fontname','Helvetica','fontsize',fSize);
-    else
-        title('RH');
+for t = 1:2
+    for s=1:length(subList)
+        for z = 1:2
+            % PERC FIGURE OR MEAN FIGURE
+            if t == 1
+                figure(percFig)
+                plotData = percSig{s}(:,:,z);
+                yMin = -.1; yMax = .6; yUnit = .1;
+                yLabel = 'significant voxels (%)';
+            else
+                figure(meanFig)
+                plotData = meanZ{s}(:,:,z);
+                yMin = -4; yMax = 4;  yUnit = 2;
+                yLabel = 'average Z-score';
+            end
+            subplot(3,2,z+(s-1)*2);
+            hold on;
+            plot(plotData,'-o');
+            ylim([yMin,yMax]); xlim([.5,18.5]);
+            if z==1
+                title(sprintf('%s: LH',subList{s}),'fontname','Helvetica','fontsize',fSize(2));
+                if s == 1
+                    legend('right hand','left hand','location','northwest')
+                    legend boxoff
+                elseif s == 2
+                    ylabel(yLabel,'fontname','Helvetica','fontsize',fSize(2));
+                else
+                end
+            else
+                title(sprintf('%s: RH',subList{s}),'fontname','Helvetica','fontsize',fSize(2));
+            end
+            set(gca,gcaOpts{:},'xtick',1:18,'xticklabel',includeROIs,'ytick',yMin:yUnit:yMax);
+            rotateXLabels(gca,45);
+            hold off
+        end
+        set(gcf, 'units', 'centimeters'); % make figure size units centimeters
+        oldPos = get(gcf, 'pos');
+        newPos = oldPos;
+        newPos(3) = 30;
+        newPos(4) = 30;
+        set(gcf, 'pos',newPos);
     end
-    set(gca,gcaOpts{:},'xtick',1:18,'xticklabel',includeROIs,'ytick',-4:2:4);
-    rotateXLabels(gca,45);
-    hold off
 end
-set(gcf, 'units', 'centimeters'); % make figure size units centimeters
-oldPos = get(gcf, 'pos');
-newPos = oldPos;
-newPos(3) = 30;
-newPos(4) = 10;
-set(gcf, 'pos',newPos);
-figName = sprintf('meanROI_%s',SUB_ID);
-export_fig([figName,'.pdf'],'-pdf','-transparent',gcf);
+
+export_fig('meanROI.pdf','-pdf','-transparent',meanFig);
+export_fig('percROI.pdf','-pdf','-transparent',percFig);
